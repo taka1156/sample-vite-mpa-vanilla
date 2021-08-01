d9a87980b346bfc494c3bee6b9c9bc0b20571307
taka <tomo05061@gmail.com>
2021-08-02 00:02:31 +0900

    update: ddの見直し

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -3,5 +3,9 @@ import tools from './utils';
 
 $('#nav-toggle').on('click', () => {
   const toggle = tools.toBoolean($('#nav-list').attr('aria-expand'));
+  const t1 = toggle;
+  const t2 = !toggle;
   $('#nav-list').attr('aria-expand', `${!toggle}`);
+  tools.dump.line('t1=>', t1, ', t2=>', t2);
+  tools.dump.row('t=>', t1, '=>', t2);
 });
--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -1,7 +1,17 @@
 /**
- * console.logを省略
+ * console.logを可変長引数対応、省略
  */
-const dd = v => console.log(v);
+const dump = {
+  row: (...args) => {
+    args.forEach(v => {
+      console.log(v);
+    });
+  },
+  line: (...args) => {
+    const lineLog = args.reduce((a, c) => a + c, '');
+    console.log(lineLog);
+  }
+};
 
 /**
  * jQueryから受け取ったものをBooleanとして返す。
@@ -10,4 +20,4 @@ const toBoolean = str => {
   return JSON.parse(str);
 };
 
-export default { dd, toBoolean };
+export default { dump, toBoolean };
